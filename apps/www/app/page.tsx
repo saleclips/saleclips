45dff4ff2fca6e19de0f97ff02fc94facee75c35
sapling <hello@sapling.dev>
2024-08-28 22:55:28 +0200

    SC-28 feat: add inngest, create client and trigger basic hello world

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
+import { inngest } from "@/inngest/client"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-4 justify-center items-center">
       <h1 className="font-semibold text-4xl">SaleClips.ai</h1>
+      <button
+        onClick={async () =>
+          await inngest.send({
+            name: "test/hello.world",
+            data: {
+              email: "testFromNext@example.com",
+            },
+          })
+        }
+      >
+        Trigger queue
+      </button>
     </main>
   )
 }
