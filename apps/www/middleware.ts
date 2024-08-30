86496951ccd1a3f2534a60f7ce92d74fc5033d8a
sapling <hello@sapling.dev>
2024-08-30 17:46:36 +0200

    SC-39 refactor: remove clerk and add a working supabase auth sample

--- apps/www/middleware.ts
+++ apps/www/middleware.ts
@@ -1,12 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server"
+import { type NextRequest } from "next/server"
+import { updateSession } from "@/utils/supabase/middleware"
 
-export default clerkMiddleware()
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }
